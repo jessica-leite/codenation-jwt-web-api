f6ebaacbf5b6933213113ed9060cd2e416d36d03
Jessica Leite <jessica.mleite@gmail.com>
2020-07-22 15:20:02 +0100

    PasswordValidatorService

--- Source/Services/PasswordValidatorService.cs
+++ Source/Services/PasswordValidatorService.cs
@@ -1,26 +1,37 @@
 using Codenation.Challenge.Models;
 using IdentityServer4.Models;
 using IdentityServer4.Validation;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Codenation.Challenge.Services
 {
-    public class PasswordValidatorService: IResourceOwnerPasswordValidator
+    public class PasswordValidatorService : IResourceOwnerPasswordValidator
     {
+        private readonly CodenationContext _context;
+
         public PasswordValidatorService(CodenationContext dbContext)
-        {            
+        {
+            _context = dbContext;
         }
 
         public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
         {
-            var userName = context.UserName;
-            var password = context.Password;
+            var user = _context.Users.FirstOrDefault(u => u.Email == context.UserName);
 
-            context.Result = new GrantValidationResult( TokenRequestErrors.InvalidGrant, "Invalid username or password");
+            var invalidUser = user == null || user.Password != context.Password;
 
+            if (invalidUser)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "Invalid username or password");
+                return Task.CompletedTask;
+            }
+
+            var claims = UserProfileService.GetUserClaims(user);
+            context.Result = new GrantValidationResult(user.Id.ToString(), "custom", claims);
 
             return Task.CompletedTask;
         }
-     
+
     }
 }
